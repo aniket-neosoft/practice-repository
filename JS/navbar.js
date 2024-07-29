cb309c0e0194e467ed8c839ebf3dad5a9a1def0c
aniket-neosoft <aniket.ramteke@neosoftmail.com>
2024-07-28 23:28:09 -0700

    Initial commit

---
+++ JS/navbar.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const navbar = document.getElementById('navbar');
+
+    const navItems = [
+        { name: 'Home', href: './index.htm' },
+        { name: 'Products', href: './product.htm' },
+        { name: 'Categories', href: './categories.htm' },
+        { name: 'About Us', href: './aboutUs.htm' },
+        { name: 'Contact', href: './contact.htm' },
+        { name: 'Cart', href: './cart.htm' }
+    ];
+
+    const ul = document.createElement('ul');
+
+    navItems.forEach(item => {
+        const li = document.createElement('li');
+        const a = document.createElement('a');
+        a.textContent = item.name;
+        a.href = item.href;
+        li.appendChild(a);
+        ul.appendChild(li);
+    });
+
+    navbar.appendChild(ul);
+});
